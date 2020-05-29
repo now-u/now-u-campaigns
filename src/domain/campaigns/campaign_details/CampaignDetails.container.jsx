c6db95d94cfff166e84d1a14f6fb8a3d4a0c6b8b
Reeann <reeannvirginia@gmail.com>
2020-05-29 09:46:38 -0400

    fix lint issues

--- src/domain/campaigns/campaign_details/CampaignDetails.container.jsx
+++ src/domain/campaigns/campaign_details/CampaignDetails.container.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
+import { campaignsURL } from '../../../utils/constants';
 import Campaign from './CampaignDetails.component';
 
 const CampaignContainer = () => {
@@ -8,12 +9,12 @@ const CampaignContainer = () => {
 
   useEffect(() => {
     const fetchCampaign = async () => {
-      const resp = await fetch(`https://now-u-api.herokuapp.com/api/v1/campaigns/${params.id}`);
+      const resp = await fetch(`${campaignsURL}/${params.id}`);
       const campaign = await resp.json();
       setCampaign(campaign.data);
     };
     fetchCampaign();
-  }, []);
+  }, [params.id]);
 
   if (!campaign) return null;
   return <Campaign campaign={campaign} />;

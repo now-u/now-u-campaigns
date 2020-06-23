d63d9909791783f766ff2ea7ecfe2b2a212d1d4c
Tejas Shah <shah.te@northeastern.edu>
2020-06-23 19:02:55 -0400

    Basic blog dev

--- src/domain/blog/Blog.jsx
+++ src/domain/blog/Blog.jsx
@@ -68,6 +68,21 @@ const Blog = () => {
                 </div>
                 <div className={classes.blogAds}>
                     <AppDownloadSquare />
+                    <BlogPost
+                        blogTitle="The impacts of climate change on the water crisis"
+                        blogAuthor="Helene McHugh"
+                        blogHashtags={["climatechange", "WASH"]}
+                        blogImageSrc="https://www.onblastblog.com/wp-content/uploads/2018/05/free-images-for-blogs.jpeg"
+                    />
+                    <BlogPost
+                        blogTitle="What do UK policies mean for refugees, asylum seekers and other migrants?"
+                        blogAuthor="Helene McHugh"
+                        blogHashtags={["refugee", "migrants", "UK"]}
+                        blogImageSrc="https://www.onblastblog.com/wp-content/uploads/2018/05/free-images-for-blogs.jpeg"
+                    />
+                </div>
+                <div className={classes.blogAds}>
+                    Blog ads
                 </div>
             </div>
             <Footer/>

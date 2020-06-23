20d887b41bbaeb0598da7604463228008cb93014
Tejas Shah <shah.te@northeastern.edu>
2020-06-23 19:02:55 -0400

    Basic blog dev

--- src/domain/blog/Blog.jsx
+++ src/domain/blog/Blog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Blog.module.scss';
 
 import { Text, Footer } from '../../components';
+import { BlogPost } from './components';
 
 const Blog = () => {
 
@@ -12,8 +13,24 @@ const Blog = () => {
                     Blog
                 </Text>
             </div>
-            <div>
-
+            <div className={classes.content}>
+                <div className={classes.blogs}>
+                    <BlogPost
+                        blogTitle="The impacts of climate change on the water crisis"
+                        blogAuthor="Helene McHugh"
+                        blogHashtags={["climatechange", "WASH"]}
+                        blogImageSrc="https://www.onblastblog.com/wp-content/uploads/2018/05/free-images-for-blogs.jpeg"
+                    />
+                    <BlogPost
+                        blogTitle="What do UK policies mean for refugees, asylum seekers and other migrants?"
+                        blogAuthor="Helene McHugh"
+                        blogHashtags={["refugee", "migrants", "UK"]}
+                        blogImageSrc="https://www.onblastblog.com/wp-content/uploads/2018/05/free-images-for-blogs.jpeg"
+                    />
+                </div>
+                <div className={classes.blogAds}>
+                    Blog ads
+                </div>
             </div>
             <Footer/>
         </div>

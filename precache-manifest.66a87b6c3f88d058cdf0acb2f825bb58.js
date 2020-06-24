2831d2913893a06a35e633f3911d874e531ba61f
jelgar <jamesnelgar@gmail.com>
2020-06-24 23:32:34 +0100

    Updates

---
+++ precache-manifest.66a87b6c3f88d058cdf0acb2f825bb58.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6398f69cd8f9b139ea84314241a5e98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90fe7bff69795311f8bd",
+    "url": "/static/css/main.841cb9bf.chunk.css"
+  },
+  {
+    "revision": "0737bed75650d18528fd",
+    "url": "/static/js/2.96b3ae91.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.96b3ae91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90fe7bff69795311f8bd",
+    "url": "/static/js/main.f332dede.chunk.js"
+  },
+  {
+    "revision": "5f382e7a5ebf09239a30",
+    "url": "/static/js/runtime-main.2f77087f.js"
+  },
+  {
+    "revision": "9377dd96daab52a0dfd5855f3536cea3",
+    "url": "/static/media/app-download.9377dd96.svg"
+  },
+  {
+    "revision": "c5dfc67956c825012728c64cea090151",
+    "url": "/static/media/apple-store.c5dfc679.png"
+  },
+  {
+    "revision": "d9335a2802ababd8b3c24a369c3c121a",
+    "url": "/static/media/google-store.d9335a28.png"
+  },
+  {
+    "revision": "23fe7730c04158d02dd17889b2067b59",
+    "url": "/static/media/now-u-logo-orange.23fe7730.svg"
+  },
+  {
+    "revision": "25c0d41bd5b522b6a80d02e0887cf05d",
+    "url": "/static/media/now-u_privacy-notice.25c0d41b.pdf"
+  },
+  {
+    "revision": "02c2e18c7df0e903acc79f4d84e00796",
+    "url": "/static/media/our_story_img.02c2e18c.png"
+  },
+  {
+    "revision": "c0d9ab35ae0800f61c6fcfc97486415b",
+    "url": "/static/media/road-sign.c0d9ab35.svg"
+  },
+  {
+    "revision": "94e6d6129c5e5f8ba89ac8a65bf6bc03",
+    "url": "/static/media/step1.94e6d612.svg"
+  },
+  {
+    "revision": "77dfcdde443dd5a0e194876e9ce64dd3",
+    "url": "/static/media/step2.77dfcdde.svg"
+  },
+  {
+    "revision": "363af922cca0b564fa27cc11fd41c15c",
+    "url": "/static/media/step3.363af922.svg"
+  },
+  {
+    "revision": "465387fd7034cea8081cb201dcc74b58",
+    "url": "/static/media/the_app.465387fd.png"
+  },
+  {
+    "revision": "0abeb98f3cf36c8d61df6b297df83664",
+    "url": "/static/media/world_illustration.0abeb98f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/now-u-campaigns/precache-manifest.7a20e0efe7dd6c66f1a2e9952a555aeb.js"
+  "/precache-manifest.66a87b6c3f88d058cdf0acb2f825bb58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/now-u-campaigns/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

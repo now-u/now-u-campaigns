ec436918ee02affdbd50fd7808de0633affa270c
Tejas Shah <shah.te@northeastern.edu>
2020-06-24 15:15:36 -0400

    Add callback for searching in the SearchBox component

--- src/components/search_box/SearchBox.jsx
+++ src/components/search_box/SearchBox.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from  './SearchBox.module.scss';
 import PropTypes from 'prop-types';
 import { searchIcon } from '../../assets';
 
-const SearchBox = ({ title, placeholder }) => {
+const SearchBox = ({ title, placeholder, onSubmit }) => {
+
+    const [searchText, setSearchText] = useState('');
+
+    const search = () => {
+        if(onSubmit) {
+            onSubmit(searchText);
+        }
+    }
+
+    const handleChange = (event) => {
+        setSearchText(event.target.value);
+    }
+
     return (
         <div>
             <div className={classes.title}>{title}</div>
             <div className={classes.input}>
-                <input type="text" placeholder={placeholder} />
-                <img src={searchIcon} />
+                <input type="text" placeholder={placeholder} value={searchText} onChange={handleChange} />
+                <img src={searchIcon} onClick={search} />
             </div>
         </div>
     );
@@ -17,7 +30,8 @@ const SearchBox = ({ title, placeholder }) => {
 
 SearchBox.propTypes = {
     title: PropTypes.string,
-    placeholder: PropTypes.string
+    placeholder: PropTypes.string,
+    onSubmit: PropTypes.func
 }
 
 export default SearchBox;

1bad6519eb324d899c0a7026f55ded8160a19d01
Tejas Shah <shah.te@northeastern.edu>
2020-06-24 16:48:53 -0400

    Create AppDownloadSquare ad component

--- src/components/index.js
+++ src/components/index.js
@@ -9,3 +9,4 @@ export { default as Footer } from './footer/Footer';
 export { default as DownloadButtons } from './download_buttons/DownloadButtons.jsx';
 export { default as Card } from './card/Card';
 export { default as SearchBox } from './search_box/SearchBox';
+export { default as AppDownloadSquare } from './app_download_square/AppDownloadSquare';

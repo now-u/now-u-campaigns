11429a29dcbb8a31f9b0b360d151ae2d6a35db02
Tejas Shah <shah.te@northeastern.edu>
2020-06-30 13:01:04 -0400

    Add contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { Campaigns, Campaign, Homepage, AboutUs, NowUApp } from './domain';
+import { Campaigns, Campaign, Homepage, AboutUs, NowUApp, GetInTouch } from './domain';
 import { AnnounceBar, TopNav, Footer } from './components';
 import './App.scss';
 
@@ -16,6 +16,7 @@ const App = () => {
           <Route path="/campaigns/:id" exact component={Campaign} />
           <Route path="/aboutus" exact component={AboutUs} />
           <Route path="/now-u-app" exact component={NowUApp} />
+          <Route path="/get-in-touch" exact component={GetInTouch} />
           <Redirect to={'/'} />
         </Switch>
       </div>
--- src/domain/index.js
+++ src/domain/index.js
@@ -3,3 +3,4 @@ export { default as Campaigns } from './campaigns/Campaigns';
 export { default as Campaign } from './campaigns/campaign_details/CampaignDetails.container';
 export { default as AboutUs } from './aboutus/AboutUs';
 export { default as NowUApp } from './nowuapp/NowUApp';
+export { default as GetInTouch } from './getintouch/GetInTouch';

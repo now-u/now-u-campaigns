c9c0ad45c94ad5f61f4adc3eb702c0eba8462e2d
nicolekwli <nicolekwli19@gmail.com>
2020-07-10 02:00:01 +0100

    faqs

--- src/domain/faqs/components/faq/FAQ.jsx
+++ src/domain/faqs/components/faq/FAQ.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './FAQ.module.scss';
 import PropTypes from 'prop-types';
 import { Text } from '../../../../components';
 
 const FAQ = ( {faq: {id, question, answer}} ) => {
+    const [isShow, setShow] = useState(false);
 
     return (
         <div key={id} className={classes.faqContainer}>
             <div className={classes.questionContainer}>
-                <div className={classes.question}>
-                    <i className="material-icons">
-                        expand_more
-                    </i>
+                <div className={classes.question} onClick = { () => setShow(!isShow)}>
+                { isShow
+                    ? <i className="material-icons"> expand_less </i>
+                    : <i className="material-icons"> expand_more </i>
+                }
                     <Text type='p'> {question} </Text>
                     
                 </div>
             </div>
 
-            <div className={classes.answerContainer}>
-                <div className={classes.answer}>
-                    <Text type='p'> {answer} </Text>
+            { isShow
+                ? <div className={classes.answerContainer}>
+                    <div className={classes.answer}>
+                        <Text type='p'> {answer} </Text>
+                    </div>
                 </div>
-            </div>
+                : null
+            }
+
+            
 
         </div>
         

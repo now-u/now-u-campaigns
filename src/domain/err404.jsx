2a4a4f6d59619d78aca324473219a6c99733aca8
Tom Streuli <tom.streuli@gmail.com>
2020-07-14 15:29:23 +0100

    404 page fix

--- src/domain/err404.jsx
+++ src/domain/err404.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './err404.module.scss';
-import { Text } from '../../components';
-import { err404 } from '../../assets';
+import { Text } from '../components';
+import { err404 } from '../assets';
 
 const error404 = () => {
     return (
         <div className={classes.box}>
+            <img src={err404} alt="Error 404"/>
             <div className={classes.text}>
                 <Text type="h1">
                     Oh Dear...
@@ -14,7 +15,6 @@ const error404 = () => {
                     Looks like something’s wrong…
                 </Text>
             </div>
-            <img src={err404} alt="Error 404"/>
         </div>
 
     );

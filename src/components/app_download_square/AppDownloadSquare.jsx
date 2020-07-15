71c55757f81b8593a8f2d101bb21f5fe45c77035
Tejas Shah <shah.te@northeastern.edu>
2020-07-15 00:28:35 -0400

    Style Square App Download ad

--- src/components/app_download_square/AppDownloadSquare.jsx
+++ src/components/app_download_square/AppDownloadSquare.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './AppDownloadSquare.module.scss';
 import { Text, Button } from '../index';
-import { appleStore, googleStore } from '../../assets';
+import { appleStore, googlePlayBadge } from '../../assets';
 
 const AppDownloadSquare = ({ adText }) => {
 
     const text = adText ?
                     adText :
-                    'Are you interested in? Start taking actions today!';
+                    'Are you interested? Start taking actions today!';
 
     return (
         <div className={classes.appDownloadContainer}>
@@ -19,11 +19,17 @@ const AppDownloadSquare = ({ adText }) => {
                 <div>
                     <Text type="p">Download here:</Text>
                     <div className={classes.downloadBtns}>
-                        <Button variant="other">
+                        <Button
+                            variant="other"
+                            onClick={()=> window.open("https://apps.apple.com/us/app/now-u/id1516126639", "_blank")}
+                        >
                             <img src={appleStore} alt="app download from apple store" />
                         </Button>
-                        <Button variant="other">
-                            <img src={googleStore} alt="app download from google store" />
+                        <Button
+                            variant="other"
+                            onClick={()=> window.open("https://play.google.com/store/apps/details?id=com.nowu.app", "_blank")}
+                        >
+                            <img src={googlePlayBadge} alt="app download from google store" />
                         </Button>
                     </div>
                 </div>

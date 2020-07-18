9c4cf1e3bac03bf22f47c860b5c69b7a080f66a3
Tom Streuli <tom.streuli@gmail.com>
2020-07-18 13:02:02 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import { Campaigns, Campaign, Homepage, AboutUs, NowUApp, GetInTouch, err404 } from './domain';
 import { AnnounceBar, TopNav, Footer } from './components';
 import './App.scss';
@@ -17,8 +17,7 @@ const App = () => {
           <Route path="/aboutus" exact component={AboutUs} />
           <Route path="/now-u-app" exact component={NowUApp} />
           <Route path="/get-in-touch" exact component={GetInTouch} />
-          <Route path="/error404" exact component={err404} />
-          <Redirect to={'/error404'} />
+          <Route exact component={err404} />
         </Switch>
       </div>
       <Footer />

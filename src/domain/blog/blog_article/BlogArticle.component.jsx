aeb636cdedc8c95fc383bf5abdd5e64c2ed24554
Tejas Shah <shah.te@northeastern.edu>
2020-07-18 14:39:24 -0400

    Add Avatar component

--- src/domain/blog/blog_article/BlogArticle.component.jsx
+++ src/domain/blog/blog_article/BlogArticle.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './BlogArticle.module.scss';
-import { Text, Button, AppDownloadRectangle } from '../../../components';
+import { Text, Button, AppDownloadRectangle, Avatar } from '../../../components';
 import { Link } from 'react-router-dom';
 
 const Blog = ({ blog }) => {
-    const { title, subtitle, hashtags, sections, campaign_id } = blog;
+    const { title, subtitle, hashtags, sections, campaign_id, user } = blog;
     const campaignURL = `/campaigns/${campaign_id}`;
 
     return (
@@ -64,6 +64,9 @@ const Blog = ({ blog }) => {
                     </Button>
                 </div>
                 <AppDownloadRectangle />
+                <div className={classes.authorInfo}>
+                    <Avatar src={user.profile_image} />
+                </div>
             </div>
         </div>
     );

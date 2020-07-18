84242e5e4ee236533fd96523f536c7605d5c2798
Tejas Shah <shah.te@northeastern.edu>
2020-07-18 01:36:56 -0400

    Add Apple and Google store URLs in constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -24,3 +24,6 @@ export const mobileNavLinks = [
 export const campaignsURL = 'https://api.now-u.com/api/v1/campaigns';
 export const faqsURL = 'https://api.now-u.com/api/v1/faqs';
 export const blogsURL = 'http://localhost:3000/blogs';
+
+export const appleStoreURL = 'https://apps.apple.com/us/app/now-u/id1516126639';
+export const googleStoreURL = 'https://play.google.com/store/apps/details?id=com.nowu.app';

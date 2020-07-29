861c6403e4561582bcc160610dc548f842f8c88c
Tejas Shah <shah.te@northeastern.edu>
2020-07-28 23:30:26 -0400

    Add CampaignAd

---
+++ src/domain/blog/components/MonthCampaigns.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import classes from './MonthCampaigns.module.scss';
+import { campaignsURL } from '../../../utils/constants';
+import { CampaignAd } from '../../../components';
+
+const MonthCampaigns = () => {
+    const [campaigns, setCampaigns] = useState([]);
+
+    useEffect(() => {
+        const fetchCampaigns = async () => {
+          const resp = await fetch(campaignsURL);
+          const campaigns = await resp.json();
+          setCampaigns(campaigns?.data);
+        };
+        fetchCampaigns();
+      }, []);
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.title}>This month campaign</div>
+            {campaigns.map(campaign => (
+                <CampaignAd campaign={campaign} key={campaign.id} />
+            ))}
+        </div>
+    );
+};
+
+
+export default MonthCampaigns;

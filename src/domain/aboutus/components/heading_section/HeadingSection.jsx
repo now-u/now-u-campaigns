4df7b6cbf5c6106ea3468da63dab6c75359e6d7a
TRU <trunangst@gmail.com>
2020-08-01 18:44:08 -0500

    changed some styles, starting commit to new branch

--- src/domain/aboutus/components/heading_section/HeadingSection.jsx
+++ src/domain/aboutus/components/heading_section/HeadingSection.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
 import classes from './HeadingSection.module.scss';
 import { Text } from '../../../../components';
-import { aboutUs } from '../../../../assets';
+// import { aboutUs } from '../../../../assets';
 
 const HeadingSection = () => {
     return (
-      <div className={classes.aboutUsSection}>
-        <div className={classes.content}>
-            <div className={classes.pageHeader}>
-                <Text type="h2" className={classes.header}>About us</Text>
-            </div>
-            <div className={classes.pageHeaderText}>
-                <Text type="p">
-                now-u is a new non-profit app that launched in July 2020 with a mission to inform, involve and inspire people to help tackle some of the world’s most pressing environmental and social problems.
-                <br/><br/>
-                The global lockdown has offered us an opportunity to take stock of what’s really important to us, and a chance to move forward together instead of falling ‘back to normal’. We believe that each of us has the power to create lasting and sustainable change, together. <b>now u!</b>
+        <div className={classes.aboutUsSection}>
+            <div className={classes.content}>
+                <Text type='h2' className={classes.header}>
+                    What is now-u?
+                </Text>
+
+                <Text type='p' className={classes.description}>
+                    We are a new non-profit app that launched in July 2020 with
+                    a mission to inform, involve and inspire people to help
+                    tackle some of the world's most pressing environmental and
+                    social problems.
                 </Text>
             </div>
         </div>
-        <div className={classes.image}>
-            <img src={aboutUs} alt="About Us" />
-        </div>
-    </div>
     );
-}
+};
+// const HeadingSection = () => {
+//     return (
+//       <div className={classes.aboutUsSection}>
+//         <div className={classes.content}>
+//             <div className={classes.pageHeader}>
+//                 <Text type="h2" className={classes.header}>About us</Text>
+//             </div>
+//             <div className={classes.pageHeaderText}>
+//                 <Text type="p">
+//                 now-u is a new non-profit app that launched in July 2020 with a mission to inform, involve and inspire people to help tackle some of the world’s most pressing environmental and social problems.
+//                 <br/><br/>
+//                 The global lockdown has offered us an opportunity to take stock of what’s really important to us, and a chance to move forward together instead of falling ‘back to normal’. We believe that each of us has the power to create lasting and sustainable change, together. <b>now u!</b>
+//                 </Text>
+//             </div>
+//         </div>
+//         <div className={classes.image}>
+//             <img src={aboutUs} alt="About Us" />
+//         </div>
+//     </div>
+//     );
+// }
 
 export default HeadingSection;

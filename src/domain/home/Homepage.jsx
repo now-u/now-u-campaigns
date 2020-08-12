d7191141544138d6b68b41d1d595320de16409c9
TRU <trunangst@gmail.com>
2020-08-12 14:38:21 -0500

    added homeMixin section, worked on intro section a little.:: checkout adjusting the buttons component for easier use, doesnt scale and has a large space

--- src/domain/home/Homepage.jsx
+++ src/domain/home/Homepage.jsx
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Intro, TheApp, ActiveCampaigns, HowItWorks } from './components/index';
 import classes from './Homepage.module.scss';
+import { Intro, TheApp, ActiveCampaigns, HowItWorks } from './components/index';
 
 const Homepage = () => {
-  const [campaigns, setCampaigns] = useState([]);
+    const [campaigns, setCampaigns] = useState([]);
 
-  useEffect(() => {
-    const fetchCampaigns = async () => {
-      const resp = await fetch('https://api.now-u.com/api/v1/campaigns');
-      const campaigns = await resp.json();
-      setCampaigns(campaigns?.data);
-    };
-    fetchCampaigns();
-  }, []);
+    useEffect(() => {
+        const fetchCampaigns = async () => {
+            const resp = await fetch('https://api.now-u.com/api/v1/campaigns');
+            const campaigns = await resp.json();
+            setCampaigns(campaigns?.data);
+        };
+        fetchCampaigns();
+    }, []);
 
-  return (
-    <div className={classes.homepageContainer}>
-      <Intro />
-      <HowItWorks />
-      <ActiveCampaigns campaigns={campaigns} />
-      <TheApp />
-    </div>
-  );
+    return (
+        <div className={classes.homepageContainer}>
+            <Intro />
+            <HowItWorks />
+            <ActiveCampaigns campaigns={campaigns} />
+            <TheApp />
+        </div>
+    );
 };
 
 export default Homepage;

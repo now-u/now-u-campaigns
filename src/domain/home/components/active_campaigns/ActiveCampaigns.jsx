24bcee5b4e8d66fceb3d9575b6d95bb38c81e7fe
TRU <trunangst@gmail.com>
2020-08-13 13:16:26 -0500

    edited mixin for nopadding in section field to remove !important:: FIND OTHER !IMPORTANT TAG I MADE EARLIER AND DO THE SAME

--- src/domain/home/components/active_campaigns/ActiveCampaigns.jsx
+++ src/domain/home/components/active_campaigns/ActiveCampaigns.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Text, Button } from '../../../../components';
 import classes from './ActiveCampaigns.module.scss';
@@ -10,7 +9,10 @@ const ActiveCampaign = ({ campaigns }) => {
     const history = useHistory();
 
     return (
-        <section className={classes.campaignContainer}>
+        <section
+            className={classes.campaignContainer}
+            id={classes.campaignContainer}
+        >
             <div className={classes.content}>
                 <div className={classes.textSection}>
                     <Text type='h2' className={classes.header}>
@@ -50,103 +52,10 @@ const ActiveCampaign = ({ campaigns }) => {
                 </Button>
             </div>
         </section>
-        // <Link key={id} to={`/campaigns/${id}`}>
-        //     <figure
-        //         key={id}
-        //         className={classes.campaignCard}
-        //         style={{
-        //             backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.6))`,
-        //         }}
-        //     >
-        //         <img
-        //             src={`url(${header_image})`}
-        //             alt={`dynamic picture of ${title}`}
-        //         />
-        //         <figcaption
-        //             className={classes.campaignTitle}
-        //         >
-        //             <Text type='h3'>{title}</Text>
-        //         </figcaption>
-        //     </figure>
-        //     {
-        //         // <div
-        //         //     key={id}
-        //         //     className={classes.campaignCard}
-        //         //     style={{
-        //         //         backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.6)), url(${header_image})`,
-        //         //     }}
-        //         // >
-        //         //     <div className={classes.campaignTitle}>
-        //         //         <Text type='h3'>{title}</Text>
-        //         //         <i className='material-icons'>
-        //         //             chevron_right
-        //         //         </i>
-        //         //     </div>
-        //         // </div>
-        //     }
-        // </Link>
-        //         );
-        //     })
-        // ) : (
-        //     <Text type='h3' className={classes.noCampaigns}>
-        //         Check back to see campaigns you can support on the
-        //         app!
-        //     </Text>
-        // )}
-        //         </div>
-        //         <Button
-        //             variant='inverted'
-        //             onClick={() => history.push('/campaigns')}
-        //         >
-        //             See all campaigns
-        //         </Button>
-        //     </div>
-        // </section>
-        // <div id="campaigns" className={classes.activeCampaignsContainer}>
-        //   <div className={classes.activeCampaignsContent}>
-        //     <div className={classes.header}>
-        //       <Text type="h1">Active campaigns</Text>
-        //       <Text type="p">Our monthly campaigns tackle different issues in partnership with trusted institutions</Text>
-        //     </div>
-        //     <div className={classes.campaignsRow}>
-        //       {campaigns ? (
-        //         campaigns.map(({ id, title, header_image }) => {
-        //           return (
-        //             <Link key={id} to={`/campaigns/${id}`}>
-        //               <div
-        //                 key={id}
-        //                 className={classes.campaign}
-        //                 style={{
-        //                   backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.6)), url(${header_image})`,
-        //                 }}
-        //               >
-        //                 <div className={classes.campaignTitle}>
-        //                   <Text type="h3">{title}</Text>
-        //                   <i className="material-icons">chevron_right</i>
-        //                 </div>
-        //               </div>
-        //             </Link>
-        //           );
-        //         })
-        //       ) : (
-        //         <Text type="h3" className={classes.noCampaigns}>
-        //           Check back to see campaigns you can support on the app!
-        //         </Text>
-        //       )}
-        //     </div>
-        //     <Button
-        //       variant="inverted"
-        //       onClick={() => history.push('/campaigns')}
-        //     >
-        //       See all campaigns
-        //     </Button>
-        //   </div>
-        // </div>
     );
 };
 
 ActiveCampaign.propTypes = {
-    // title: PropTypes.string.isRequired,
     campaigns: PropTypes.array || null,
 };
 

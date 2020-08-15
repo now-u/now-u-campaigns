e91084e81e4e6f85dca5ccf04dcfc819945e32d1
TRU <trunangst@gmail.com>
2020-08-14 20:56:01 -0500

    added 2 states for newsletter, added functionality with valid emails

--- src/domain/home/components/newsletter/Newsletter.jsx
+++ src/domain/home/components/newsletter/Newsletter.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Button } from '../../../../components';
 import classes from './Newsletter.module.scss';
 
+const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
 const Newsletter = () => {
+    const [formText, setFormText] = useState('');
+    const [isValid, setIsValid] = useState(false);
+
+    const handleChange = (event) => {
+        setFormText(event.target.value.trim());
+        setIsValid(emailRegex.test(formText));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(formText);
+    };
+
     return (
         <section className={classes.newsletterContainer}>
             <form
                 htmlFor='newsletter'
                 className={classes.formSection}
                 action='#'
-                method='post'
+                // method='post'
             >
                 <Text type='h2' className={classes.header}>
                     Now-u newsletter
@@ -39,12 +54,21 @@ const Newsletter = () => {
                     </li>
                 </ul>
                 <fieldset>
-                    <input type='text' placeholder='email' value='' />
+                    <input
+                        type='text'
+                        placeholder='Enter e-mail'
+                        onChange={(event) => handleChange(event)}
+                        value={formText}
+                    />
                     <Button
-                        className={classes.submitButton}
+                        className={
+                            isValid
+                                ? `${classes.submitButton}`
+                                : `${classes.submitButton} ${classes.disabled}`
+                        }
                         type='submit'
                         variant='primary'
-                        onClick={() => '#'}
+                        onClick={(event) => handleSubmit(event)}
                     >
                         Subscribe
                     </Button>

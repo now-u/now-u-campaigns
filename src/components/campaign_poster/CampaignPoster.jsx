fda2d4dc9032b4e382da5e9b9b6074a55e03814e
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-08-19 17:03:08 +0100

    Campaigns page redesign started for web

--- src/components/campaign_poster/CampaignPoster.jsx
+++ src/components/campaign_poster/CampaignPoster.jsx
@@ -4,22 +4,21 @@ import PropTypes from 'prop-types';
 import { Text, Button } from '../../components';
 import classes from './CampaignPoster.module.scss';
 
-const CampaignPoster = ({ campaign: { id, title = 'Coming soon..', number_of_campaigners, header_image } }) => {
+const CampaignPoster = ({ campaign: { id, title = 'Coming soon..', header_image, description_web } }) => {
+  const description = description_web.split('<br>')[0]
   return (
-    <div key={id} className={classes.campaignPoster} style={{ backgroundImage: `url(${header_image})` }}>
-      <Text type="h3">{title}</Text>
-      {number_of_campaigners > 1 && (
-        <Text className={classes.participants} type="h4">
-          <i className="material-icons">group</i>
-          &nbsp;{number_of_campaigners} people have joined
-        </Text>
-      )}
-      <Link to={`/campaigns/${id}`}>
-        <Button id={id} className={classes.campaignBtn} variant="primary">
-          See more
-          <i className="material-icons">chevron_right</i>
-        </Button>
-      </Link>
+    <div key={id} className={classes.campaignPoster}>
+      <div className={classes.left} style={{ backgroundImage: `url(${header_image})`, backgroundSize: 'cover'}}>
+      </div>
+      <div className={classes.right}>
+        <Text type="h4">{title}</Text>
+        <Text type="p" className={classes.description}>{description}</Text>
+        <Link to={`/campaigns/${id}`}>
+          <Button id={id} className={classes.campaignBtn} variant="primary">
+            Learn more
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/domain/campaigns/Campaigns.jsx
+++ src/domain/campaigns/Campaigns.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { AppDownload, CampaignsGroup } from '../../components';
+import { CampaignsGroup, AppDownloadButtons } from '../../components';
 import { campaignsURL } from '../../utils/constants';
 import classes from './Campaigns.module.scss';
 
 const Campaigns = () => {
   const [campaigns, setCampaigns] = useState([]);
-  const [upcomingCampaigns] = useState(null);
 
   useEffect(() => {
     const fetchCampaigns = async () => {
@@ -17,9 +16,16 @@ const Campaigns = () => {
   }, []);
   return (
     <div className={classes.campaignsContainer}>
+      <div className={classes.campaignsHeader}>
+        <h4 className={classes.campaignsTitle}>Campaigns</h4>
+        <div className={classes.appLinks}>
+        <AppDownloadButtons />
+        </div>
+      </div>
+      <div className={classes.campaignsAbout}>
+        <p>We partner with trusted charities and social enterprises to bring you focused monthly campaigns with clear and attainable objectives. Each campaign contains informative resources and actions you can complete, curated to inform, involve and inspire now-u users to make a meaningful contribution to each charitable cause.</p>
+      </div>
       <CampaignsGroup title="Active Campaigns" campaigns={campaigns} />
-      {upcomingCampaigns && <CampaignsGroup title="Upcoming Campaigns" campaigns={upcomingCampaigns} />}
-      <AppDownload />
     </div>
   );
 };

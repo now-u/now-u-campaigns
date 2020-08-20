228f5711271c4416a99cd4468c83b219230b84da
tunangst <trunangst@gmail.com>
2020-08-20 16:42:11 -0500

    fixed broken partner links

--- src/domain/home/components/partners/Partners.jsx
+++ src/domain/home/components/partners/Partners.jsx
@@ -5,53 +5,53 @@ import classes from './Partners.module.scss';
 import PartnerCard from './partner_card/PartnerCard';
 
 const Partners = ({ partners }) => {
-    return (
-        <section id='partners' className={classes.partnersContainer}>
-            <article className={classes.textSection}>
-                <Text type='h1' className={classes.header}>
-                    Our Partners
-                </Text>
-                <Text type='p' className={classes.description}>
-                    We work with many amazing charities and social enterprises.
-                    Want to find out more about partnering with now-u? Download
-                    our{' '}
-                    <a
-                        href={
-                            'https://share.now-u.com/partners/now-u_charity_collaboration_&_partnership_guide.pdf'
-                        }
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        partnership guide.
-                    </a>
-                </Text>
-            </article>
-            <div className={classes.cardSection}>
-                {partners ? (
-                    partners.map(({ name, id, logo_link }) => {
-                        return (
-                            <PartnerCard
-                                key={id}
-                                name={name}
-                                id={id}
-                                logo_link={logo_link}
-                            />
-                        );
-                    })
-                ) : (
-                    <div>
-                        <Text type='p' className={classes.loadingMsg}>
-                            Loading...
-                        </Text>
-                    </div>
-                )}
-            </div>
-        </section>
-    );
+  return (
+    <section id="partners" className={classes.partnersContainer}>
+      <article className={classes.textSection}>
+        <Text type="h1" className={classes.header}>
+          Our Partners
+        </Text>
+        <Text type="p" className={classes.description}>
+          We work with many amazing charities and social enterprises. Want to
+          find out more about partnering with now-u? Download our{' '}
+          <a
+            href={
+              'https://share.now-u.com/partners/now-u_charity_collaboration_&_partnership_guide.pdf'
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            partnership guide.
+          </a>
+        </Text>
+      </article>
+      <div className={classes.cardSection}>
+        {partners ? (
+          partners.map(({ name, id, logo_link, website }) => {
+            return (
+              <PartnerCard
+                key={id}
+                name={name}
+                id={id}
+                logo_link={logo_link}
+                website={website}
+              />
+            );
+          })
+        ) : (
+          <div>
+            <Text type="p" className={classes.loadingMsg}>
+              Loading...
+            </Text>
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 Partners.propTypes = {
-    partners: PropTypes.array || null,
+  partners: PropTypes.array || null,
 };
 
 export default Partners;

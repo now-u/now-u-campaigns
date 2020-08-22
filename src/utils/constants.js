e8fc0c6ff21694066f9cc05ebd3b6c8941bbaa2f
TRU <trunangst@gmail.com>
2020-08-22 16:46:02 -0500

    added the topNavLinks in constants, added the $topNavColor to _variables, worked slightly on TopNav module and styling: nested styling, built new topNavContent section...

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,34 +1,40 @@
 // import { privacyNotice } from '../assets';
 
+export const topNavLinks = [
+    { display: 'FAQ', path: '/faqs' },
+    { display: 'Blog', path: '/blog' },
+    { display: 'Newsletter', path: '/newsletter' },
+];
+
 export const navLinks = [
-  { display: 'Campaigns', path: '/campaigns' },
-  { display: 'Now-u app', path: '/now-u-app' },
-  { display: 'About us', path: '/aboutus' },
-  { display: 'Blog', path: '/blog' },
-  { display: 'Get in touch', path: '/get-in-touch' },
-  // { display: 'Privacy Policy', external: privacyNotice },
-  { display: "Press", path: "/press" },
-  { display: "FAQ", path: "/faqs" },
+    { display: 'Campaigns', path: '/campaigns' },
+    { display: 'Now-u app', path: '/now-u-app' },
+    { display: 'About us', path: '/aboutus' },
+    { display: 'Get in touch', path: '/get-in-touch' },
+    { display: 'Press', path: '/press' },
 ];
 
 export const mobileNavLinks = [
-  { display: "Campaigns", path: "/campaigns" },
-  { display: "Now-u app", path: "/now-u-app" },
-  { display: "About us", path: "/aboutus" },
-  { display: 'Blog', path: '/blog' },
-  { display: "Get in touch", path: "/get-in-touch" },
-  { display: "Press", path: "/press" },
-  { display: "FAQ", path: "/faqs" },
-  // { display: 'Newsletter', path: '/press' },
-  // { display: 'Privacy Policy', external: privacyNotice },
+    { display: 'Campaigns', path: '/campaigns' },
+    { display: 'Now-u app', path: '/now-u-app' },
+    { display: 'About us', path: '/aboutus' },
+    { display: 'Get in touch', path: '/get-in-touch' },
+    { display: 'FAQ', path: '/faqs' },
+    { display: 'Press', path: '/press' },
+    { display: 'Blog', path: '/blog' },
+    { display: 'Newsletter', path: '/newsletter' },
+    { display: 'Privacy Policy' },
+    // { display: 'Privacy Policy', external: privacyNotice },
 ];
 
-export const campaignsURL = "https://api.now-u.com/api/v1/campaigns";
-export const pressCoverageURL = "https://api.now-u.com/api/v1/press_coverage";
-export const faqsURL = "https://api.now-u.com/api/v1/faqs";
+export const campaignsURL = 'https://api.now-u.com/api/v1/campaigns';
+export const pressCoverageURL = 'https://api.now-u.com/api/v1/press_coverage';
+export const faqsURL = 'https://api.now-u.com/api/v1/faqs';
 export const blogsURL = 'https://api.now-u.com/api/v1/blogs';
 export const appleStoreURL = 'https://apps.apple.com/us/app/now-u/id1516126639';
-export const googleStoreURL = 'https://play.google.com/store/apps/details?id=com.nowu.app';
-export const privacyPolicyURL = "http://share.now-u.com/legal/now-u_privacy_policy.pdf";
-export const termsAndConditionsURL = "https://share.now-u.com/legal/now-u_users_Ts&Cs.pdf";
-
+export const googleStoreURL =
+    'https://play.google.com/store/apps/details?id=com.nowu.app';
+export const privacyPolicyURL =
+    'http://share.now-u.com/legal/now-u_privacy_policy.pdf';
+export const termsAndConditionsURL =
+    'https://share.now-u.com/legal/now-u_users_Ts&Cs.pdf';

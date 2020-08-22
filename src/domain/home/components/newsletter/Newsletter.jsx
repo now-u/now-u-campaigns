9b83f8b8f45e9d674032f64e9ce3bae1675ee5fd
TRU <trunangst@gmail.com>
2020-08-22 02:06:32 -0500

    removed original newsletter modules and removed comments

--- src/domain/home/components/newsletter/Newsletter.jsx
+++ src/domain/home/components/newsletter/Newsletter.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-// import { Text, Button } from '../../../../components';
 import classes from './Newsletter.module.scss';
-// import modalClasses from '../../../../components/newsletter-signup-modal/NewsletterSignupModal.module.scss';
 import PropTypes from 'prop-types';
 
-// const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-
 const Newsletter = ({ children }) => {
-    /* eslint-disable no-console */
-    // console.log(children);
-    /* eslint-enable no-console */
-
     return (
         <section className={classes.newsletterContainer}>
             <div className={classes.formSection}>{children}</div>

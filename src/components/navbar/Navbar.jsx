047a39d9b513a7cf1e504c0a7b90a86be2e3324c
TRU <trunangst@gmail.com>
2020-08-27 10:02:23 -0500

    put newsModal in dependencies of useEffect

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = ({ newsModal }) => {
             newsModal(true);
             toggleNewsModal();
         }
-    }, [showNewsModal]);
+    }, [showNewsModal, newsModal]);
 
     return (
         <header className={classes.navContainer}>

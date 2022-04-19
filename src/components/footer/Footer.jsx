bac22f75cea723280f1f94ebc519590c73016aa5
Beca Parker <beca.parker@red-gate.com>
2022-04-19 20:49:07 +0100

    WIP Different footer for mobile

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,58 +4,65 @@ import { nowUOrange } from "../../assets";
 import { AppDownloadButtons } from "../../components";
 import {footerLinks} from "../../utils/constants";
 
+
 const Footer = () => {
-  
-  return (
-    <div className={classes.footerBackground}>
-      <div className={classes.logo}>
-        <img src={nowUOrange} alt="logo" />
-      </div>
-      <div className={classes.footer}>
-        <div className={classes.footerLeft}>
-          {footerLinks.map(link => {
-            return (
-              <div className={classes.links} key={link.id}>
-                <a href={link.href} target={link.newtab ? "_blank" : null} title={link.newtab ? "Opens PDF in a new tab" : null} className={classes.links}>{link.name}</a>
-                {link.id == footerLinks.length ? null : "|"}
-              </div>
-            );
-          })}
-          <div className={classes.footerBottom}>Registered charity number: 1196568</div>
+  if (window.screen.width >= 1280) { /* the viewport is 1280 pixels wide or more */ 
+    return (
+      <div className={classes.footerBackground}>
+        <div className={classes.logo}>
+          <img src={nowUOrange} alt="logo" />
         </div>
-        <div className={classes.footerRight}>
-          <div>
-            <a
-              href="https://www.instagram.com/now_u_app/"
-              className="fa fa-instagram fa-2x"
-              target="_blank"
-              rel="noopener noreferrer"
-            ></a>
-            <a
-              href="https://www.facebook.com/nowufb"
-              className="fa fa-facebook fa-2x"
-              target="_blank"
-              rel="noopener noreferrer"
-            ></a>
-            <a href="https://twitter.com/now_u_app" 
-              className="fa fa-twitter fa-2x" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            ></a>
-            <a
-              href="https://www.linkedin.com/company/now-u/?viewAsMember=true"
-              className="fa fa-linkedin fa-2x"
-              target="_blank"
-              rel="noopener noreferrer"
-            ></a>
+        <div className={classes.footer}>
+          <div className={classes.footerLeft}>
+            {footerLinks.map(link => {
+              return (
+                <div className={classes.links} key={link.id}>
+                  <a href={link.href} target={link.newtab ? "_blank" : null} title={link.newtab ? "Opens PDF in a new tab" : null} className={classes.links}>{link.name}</a>
+                  {link.id == footerLinks.length ? null : "|"}
+                </div>
+              );
+            })}
+            <div className={classes.footerBottom}>Registered charity number: 1196568</div>
           </div>
-          <div className={classes.appDownloadLinks}>
-            <AppDownloadButtons />
+          <div className={classes.footerRight}>
+            <div>
+              <a
+                href="https://www.instagram.com/now_u_app/"
+                className="fa fa-instagram fa-2x"
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+              <a
+                href="https://www.facebook.com/nowufb"
+                className="fa fa-facebook fa-2x"
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+              <a href="https://twitter.com/now_u_app"
+                className="fa fa-twitter fa-2x"
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+              <a
+                href="https://www.linkedin.com/company/now-u/?viewAsMember=true"
+                className="fa fa-linkedin fa-2x"
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+            </div>
+            <div className={classes.appDownloadLinks}>
+              <AppDownloadButtons />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  } else {
+    return (
+      <div>Different footer for mobile
+      </div>  
+    );
+  }};
+
 
 export default Footer;

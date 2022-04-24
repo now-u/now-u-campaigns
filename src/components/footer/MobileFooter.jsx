61ebcbef602b717b0a82b6072509da620b9ebbc4
Beca Parker <beca.parker@red-gate.com>
2022-04-24 09:53:47 +0100

    WIP use @media logic to alternate footer for mobile vs. web

---
+++ src/components/footer/MobileFooter.jsx
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import classes from "./Footer.module.scss";
+
+const MobileFooter = () => {
+
+  return (
+    <div className={classes.mobileFooter}>
+        Awesome mobile footer here
+    </div>
+  );
+};
+
+export default MobileFooter;
---
+++ src/components/footer/WebFooter.jsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import classes from "./Footer.module.scss";
+
+const WebFooter = () => {
+  return (
+    <div className={classes.webFooter}>
+      Awesome webiness here
+    </div>
+  );
+};
+
+export default WebFooter;

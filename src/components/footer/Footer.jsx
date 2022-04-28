2b8b61f642ad27775c803014c9934383cf37d7dc
Beca Parker <beca.parker@red-gate.com>
2022-04-28 17:21:50 +0100

    Browser footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import WebFooter from "./WebFooter.jsx";
 import MobileFooter from "./MobileFooter.jsx";
 
-
-/*
-import classes from "./Footer.module.scss";
-import { nowUOrange } from "../../assets";
-import { AppDownloadButtons } from "../../components";
-import {footerLinks} from "../../utils/constants";
-*/
-
-
 const Footer = () => {
   return (
     <div>
@@ -19,77 +10,5 @@ const Footer = () => {
     </div>
   );
 };
-
-// const Footer = () => {
-//   if (window.screen.width >= 1280) { /* the viewport is 1280 pixels wide or more */ 
-//     return (
-//       <div className={classes.footerBackground}>
-//         <div className={classes.logo}>
-//           <img src={nowUOrange} alt="logo" />
-//         </div>
-//         <div className={classes.footer}>
-//           <div className={classes.footerLeft}>
-//             {footerLinks.map(link => {
-//               return (
-//                 <div className={classes.links} key={link.id}>
-//                   <a href={link.href} target={link.newtab ? "_blank" : null} title={link.newtab ? "Opens PDF in a new tab" : null} className={classes.links}>{link.name}</a>
-//                   {link.id == footerLinks.length ? null : "|"}
-//                 </div>
-//               );
-//             })}
-//             <div className={classes.footerBottom}>The now-u brand encompasses now-u CIC (registered UK company number: 12709184) and now-u community CIO (registered UK charity number: 1196568)</div>
-//           </div>
-//           <div className={classes.footerRight}>
-//             <div>
-//               <a
-//                 href="https://www.instagram.com/now_u_app/"
-//                 className="fa fa-instagram fa-2x"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               ></a>
-//               <a
-//                 href="https://www.facebook.com/nowufb"
-//                 className="fa fa-facebook fa-2x"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               ></a>
-//               <a href="https://twitter.com/now_u_app"
-//                 className="fa fa-twitter fa-2x"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               ></a>
-//               <a
-//                 href="https://www.linkedin.com/company/now-u/?viewAsMember=true"
-//                 className="fa fa-linkedin fa-2x"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               ></a>
-//             </div>
-//             <div className={classes.appDownloadLinks}>
-//               <AppDownloadButtons />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   } else {
-//     return (
-//       <div className={classes.footerBackground}>
-//         <div className={classes.logo}>
-//           <img src={nowUOrange} alt="logo" />
-//         </div>
-//         <div className={classes.footer}>
-//           Insert the footernavs here as columns
-//         </div>
-//         <div className={classes.appDownloadLinksForMobile}>
-//           <AppDownloadButtons />
-//         </div>
-//         <div className={classes.footerBottom}>
-//           The now-u brand encompasses now-u CIC (registered UK company number: 12709184) and now-u community CIO (registered UK charity number: 1196568)
-//         </div>
-//       </div>
-//     );
-//   }};
-
 
 export default Footer;

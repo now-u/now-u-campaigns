f963156544f0568f16b28bd3b8a83d0321831ee2
Beca Parker <beca.parker@red-gate.com>
2022-04-28 17:34:45 +0100

    WIP mobile footer

--- src/components/footer/MobileFooter.jsx
+++ src/components/footer/MobileFooter.jsx
@@ -1,11 +1,41 @@
 ﻿import React from "react";
 import classes from "./Footer.module.scss";
+import {nowUOrange} from "../../assets";
+import {AppDownloadButtons} from "../index";
+import {footerLinks} from "../../utils/constants";
 
 const MobileFooter = () => {
 
   return (
     <div className={classes.mobileFooter}>
-        Awesome mobile footer here
+      <div className={classes.footerBackground}>
+        <div className={classes.logo}>
+          <img src={nowUOrange} alt="logo" />
+        </div>
+        <div className={classes.footer}>
+          <div className={classes.linkContainer}>
+            {footerLinks.map(link => {
+              return (
+                <div className={classes.links} key={link.id}>
+                  <a href={link.href} target={link.newtab ? "_blank" : null} title={link.newtab ? "Opens PDF in a new tab" : null} className={classes.links}>{link.name}</a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={classes.footer}>
+          <a href="https://www.instagram.com/now_u_app/" className="fa fa-instagram fa-2x" target="_blank" rel="noopener noreferrer"/>
+          <a href="https://www.facebook.com/nowufb" className="fa fa-facebook fa-2x" target="_blank" rel="noopener noreferrer"/>
+          <a href="https://twitter.com/now_u_app" className="fa fa-twitter fa-2x" target="_blank" rel="noopener noreferrer"/>
+          <a href="https://www.linkedin.com/company/now-u/?viewAsMember=true" className="fa fa-linkedin fa-2x" target="_blank" rel="noopener noreferrer"/>
+        </div>
+        <div className={classes.appDownloadLinksForMobile}>
+          <AppDownloadButtons />
+        </div>
+        <div className={classes.footerBottom}>
+          The now-u brand encompasses now-u CIC (registered UK company number: 12709184) and now-u community CIO (registered UK charity number: 1196568)
+        </div>
+      </div>
     </div>
   );
 };
